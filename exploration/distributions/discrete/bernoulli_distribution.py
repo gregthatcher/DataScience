ed893838303b9191d386c30ba94f100f48c53c19
Greg Thatcher <greg@gregthatcher.com>
2021-04-23 10:07:20 -0700

    Graph improvements

--- exploration/distributions/discrete/bernoulli_distribution.py
+++ exploration/distributions/discrete/bernoulli_distribution.py
@@ -15,11 +15,14 @@ plt.style.use("ggplot")
 
 fig.suptitle("Bernoulli Distribution: P(x; p) = (px) + (1-p)(1-x); "\
     "used for 1 trial with two outcomes.\n"\
+    "Variance is p(1-p); "\
     "x is 0 or 1; Var[X] = E[X^2]-E[X]^2")
 
 x = np.arange(-1, 2, 0.5)
-for index, p in enumerate([0.3, 0.5, 0.7]):
+for index, p in enumerate([0.3, 0.5, 1/200]):
     ax[index].vlines(x, 0, stats.bernoulli.pmf(x, p), colors='b', lw=5, alpha=0.5)
     ax[index].set_title(f"p={p}")
-
+    if p == 0.5:
+        ax[index].text(0.25, 0.505, "Flipping a coin", horizontalalignment='center')
+        
 plt.show()
