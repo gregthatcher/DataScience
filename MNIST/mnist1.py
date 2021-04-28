62e4b285d5781b525621675432f208e02ddfb3cc
Greg Thatcher <greg@gregthatcher.com>
2021-04-28 16:41:22 -0700

    First version that can predict

--- MNIST/mnist1.py
+++ MNIST/mnist1.py
@@ -6,6 +6,12 @@ https://towardsdatascience.com/mnist-cnn-python-c61a5bce7a19
 from keras.datasets import mnist
 import keras
 from keras.utils import to_categorical
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.models import Sequential
+import numpy as np
+import matplotlib.pyplot as plt
+
+MODEL_PATH = "./models/minst1.model"
 
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 #from mnist import MNIST
@@ -23,5 +29,42 @@ train_X = train_X / 255
 test_X = test_X / 255
 
 # Use one-hot encoding (if we were using DataFrames, we could use "dummies")
-train_Y_one_hot = to_categorical(train_Y)
-test_Y_one_hot = to_categorical(test_Y)
+train_Y_one_hot = to_categorical(train_y)
+test_Y_one_hot = to_categorical(test_y)
+print(train_Y_one_hot.shape)
+print(test_Y_one_hot.shape)
+
+
+try:
+    model = keras.models.load_model(MODEL_PATH)
+except OSError:
+    model = Sequential()
+    model.add(Conv2D(64, (3,3), input_shape=(28, 28, 1)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(64, (3,3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+    model.fit(train_X, train_Y_one_hot, batch_size=64, epochs=2)
+
+    model.save(MODEL_PATH)
+
+test_loss, test_acc = model.evaluate(test_X, test_Y_one_hot)
+print('Test loss', test_loss)
+print('Test accuracy', test_acc)
+
+predictions = model.predict(test_X)
+final_prediction = np.argmax(np.round(predictions[0]))
+print(final_prediction)
+
+
+plt.imshow(test_X[0], cmap=plt.get_cmap('gray'))
+plt.title(f"Model Predicted {final_prediction}")
+
+plt.show()
