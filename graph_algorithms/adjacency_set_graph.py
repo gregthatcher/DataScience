3ad72321c97e2bd5673e4b749b3420f65e6ddb1a
Greg Thatcher <greg@gregthatcher.com>
2021-05-25 10:21:01 -0700

    Starting Adjacency sets

---
+++ graph_algorithms/adjacency_set_graph.py
@@ -0,0 +1,6 @@
+'''
+Adjacency Sets are an alternative to Adjaceny Matrices
+Note that we can also make an "Adjaceny List", but
+lists aren't as effective as sets since sets
+can delete and search items faster.
+'''
