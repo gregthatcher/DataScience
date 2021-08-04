b04e6c44a99b13fb0ff4cd76dcbf6b0ac4e5833b
Greg Thatcher <greg@gregthatcher.com>
2021-08-04 08:57:46 -0700

    Laplaces Rule of Succession, part 2

--- mathematics/probability/laplaces_rule_of_succession.py
+++ mathematics/probability/laplaces_rule_of_succession.py
@@ -8,12 +8,25 @@ the same price.  Which seller should you choose?
 93% with 200 ratings
 Laplaces Rule of Succession gives you a way to calculate this
 You just pretend to add 1 positive review and 1 negative review
-(2 reviews total), and then recacalcuate the rating
+(2 reviews total), and then recacalcuate the rating to get
+the expected rating
 '''
 
 
 def laplace_rule_of_succession(global_rating, max_rating,
                                total_number_of_reviews):
+    '''
+    If you look at a review for an Amazon item, look for text
+    like this: "4.2 out of 5... 52 global ratings"
+    In this case, global_rating would be 4.2, max_rating would be 5,
+    and total_number_of_reviews would be 52
+    Pass these numbers in, and this function will calculate
+    a kind of normalized rating so you can compare ratings
+    of other sellers with different numbers of reviews.
+    e.g. 100% positive for 10 reviews for one seller may be worse than
+    98% positive with 50 reviews for another seller.
+    '''
+
     percent_positive = global_rating/max_rating
     print(f"Percent Positive {percent_positive * 100}")
     # Now, let's pretend that we had two more reviews,
@@ -27,6 +40,10 @@ def laplace_rule_of_succession(global_rating, max_rating,
     print()
 
 
+# These came from video, https://www.youtube.com/watch?v=8idr1WZ1A7Q&t=327s
 laplace_rule_of_succession(5, 5, 10)
 laplace_rule_of_succession((48/50) * 5, 5, 50)
 laplace_rule_of_succession((186/200) * 5, 5, 200)
+# Amazon rating was 4.2 out of 5 with 52 global ratings
+# These rating are for the book "How to Box" by Joe Louis
+laplace_rule_of_succession(4.2, 5, 52)
