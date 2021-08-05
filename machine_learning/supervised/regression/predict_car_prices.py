18e87948389db066ee4e0b0811e22ea57df852f1
Greg Thatcher <greg@gregthatcher.com>
2021-08-04 19:01:47 -0700

    Ridge Regression

--- machine_learning/supervised/regression/predict_car_prices.py
+++ machine_learning/supervised/regression/predict_car_prices.py
@@ -8,7 +8,10 @@ Note that regression models are considered to be a High Bias Algorithm
 high Variance Algorithms)
 In high Variance algorithms, parameters are complex
 In high Bias algorithms, parameters are simple but
-assumptions about model are great (e.g. "model is linear")
+assumptions about model are great (e.g. "model is linear").
+
+TODO: Note that in addition to Lasso and Ridge (I have examples below),
+we also hae Elastic Net Regression which combines both Lasso and Ridge.
 '''
 
 import math
@@ -86,20 +89,37 @@ y = auto_data["price"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=11)
 
-
+# Note that adding normalize=True makes model go 
+# crazy, why?  It works below for Lasso??
+# linear_model = LinearRegression(normalize=True)
 linear_model = LinearRegression()
 linear_model.fit(X_train, y_train)
 analyze_model(linear_model, "Linear Regression",
               X_train, y_train, X_test, y_test)
 
-# Alpha is regularization parameter
-# This is multipled by L1 norm (sum of absolute values)
-# by normalizing, we are "centering" by subtracting the mean
+# Lasso ~ Least Absolute Shrinkage and Selection Operator
+#  Alpha is regularization parameter
+# "Regularization" is one way to combat
+# complex (high variance) models.
+# Regularization reduces variance error, but
+# increases Bias error.
+# Alpha is multipled by L1 norm (sum of absolute values) and
+# added to objective function which previously only
+# tried to minimize least squares (sqrt((y_actual-y_predicted)^2))
+# so we end up with
+# (sqrt((y_actual-y_predicted)^2))
+# + Alpha * (|First Coefficient|^1 + [Second Coefficient]^1 + ...)
+# That is, we add the L1 norm of the coefficients as a penalty.
+#
+# By normalizing, we are "centering" by subtracting the mean
 # and dividing by L2 norm
+#
 # Note that Lasso regression will reduce some
 # coefficients to zero;  we can remove these
 # features if we're sure they weren't colinear with
-# other features.
+# other features.  This is one way to find
+# out which features are important or not.
+# By tuning Alpha, we can elimate unimportant features.
 lasso_model = Lasso(alpha=0.5, normalize=True)
 lasso_model.fit(X_train, y_train)
 analyze_model(lasso_model, "Lasso - Alpha=0.5",
@@ -111,3 +131,26 @@ lasso_model = Lasso(alpha=5, normalize=True)
 lasso_model.fit(X_train, y_train)
 analyze_model(lasso_model, "Lasso - Alpha = 5",
               X_train, y_train, X_test, y_test)
+
+# For Ridge Regression, we modify the objective function
+# as follows:
+# (sqrt((y_actual-y_predicted)^2))
+# + Alpha * (|First Coefficieng|^2 + [Second Coefficient]^2 + ...)
+# That is, we add the L2 norm of the coefficients as a penalty.
+#
+# Unlike Lasso, tuning the alpha parameter will not
+# force (unimportant feature) coefficients to zero, so its
+# not suitable for finding unimportant (linear) features.
+#
+# The absolute value in the L1 norm makes it impossible
+# to find a "closed form solution".  This means that
+# Lasso tries many models to find a solution.  In contrast,
+# Ridge Regression _does_ have a closed form solution
+# (it can be expressed/calculated as a formula),
+# so it only tries one model.
+# TODO: Let's try some "hyperparamter tuning" to find best Alpha
+for alpha in [0.05, 0.5]:
+    ridge_model = Ridge(alpha=alpha, normalize=True)
+    ridge_model.fit(X_train, y_train)
+    analyze_model(ridge_model, f"Ridge - Alpha = {alpha}",
+                  X_train, y_train, X_test, y_test)
