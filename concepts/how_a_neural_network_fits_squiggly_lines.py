a558d1292e09dcea6ac3e5a1b980f91e51a92741
Greg Thatcher <greg@gregthatcher.com>
2022-01-16 10:12:10 -0800

    drawing original points on graph

--- concepts/how_a_neural_network_fits_squiggly_lines.py
+++ concepts/how_a_neural_network_fits_squiggly_lines.py
@@ -52,7 +52,8 @@ def draw_activation_part(x, ax, activation_function, weight, bias, title):
     ax.set_title(title)
     height = max(activation_function(x1), activation_function(x2))
     ax.add_patch(
-        Rectangle((x2, 0), abs(x2 - x1), height, facecolor="none", edgecolor="red")
+        Rectangle((x2, 0), abs(x2 - x1), height,
+                  facecolor="none", edgecolor="red")
     )
 
 
@@ -69,6 +70,51 @@ def display_portions_of_activation_functions(
     ax.legend()
 
 
+def display_final_sums(
+    weight_1, bias_1, weight_2, bias_2, activation_function, ax, data_x,
+    output_weight_1, output_weight_2
+):
+    first_x = (data_x * weight_1) + bias_1
+    second_x = (data_x * weight_2) + bias_2
+
+    first_part = output_weight_1 * activation_function(first_x)
+    ax.plot(data_x, first_part,
+            label="1st Node Weights")
+    second_part = output_weight_2 * activation_function(second_x)
+    ax.plot(data_x, second_part,
+            label="2nd Node Weights")
+    ax.plot(data_x, first_part + second_part,
+            label="Final Sum")
+    ax.legend()
+
+
+def draw_original_points(ax):
+    size = 0.1
+    color = "yellow"
+    alpha = 1
+    edge_color = "black"
+    line_width = 1
+    radius = 0.05
+    ax.add_patch(plt.Circle((0, 0), radius=radius, facecolor=color,
+                 alpha=alpha, edgecolor=edge_color, linewidth=line_width))
+    ax.add_patch(plt.Circle((0.05, 0), radius=radius, facecolor=color,
+                 alpha=alpha, edgecolor=edge_color, linewidth=line_width))
+    ax.add_patch(plt.Circle((0.1, 0), radius=radius, facecolor=color,
+                 alpha=alpha, edgecolor=edge_color, linewidth=line_width))
+
+    ax.add_patch(plt.Circle((0.5, 1.0), radius=radius, facecolor=color,
+                 alpha=alpha, edgecolor=edge_color, linewidth=line_width))
+    ax.add_patch(plt.Circle((0.55, 1.0), radius=radius, facecolor=color,
+                 alpha=alpha, edgecolor=edge_color, linewidth=line_width))
+
+    ax.add_patch(plt.Circle((1, 0), radius=radius, facecolor=color,
+                 alpha=alpha, edgecolor=edge_color, linewidth=line_width))
+    ax.add_patch(plt.Circle((1.05, 0), radius=radius, facecolor=color,
+                 alpha=alpha, edgecolor=edge_color, linewidth=line_width))
+    ax.add_patch(plt.Circle((1.1, 0), radius=radius, facecolor=color,
+                 alpha=alpha, edgecolor=edge_color, linewidth=line_width))
+
+
 plt.style.use("seaborn")
 
 fig, ax = plt.subplots(2, 4, figsize=(14, 10))
@@ -78,14 +124,19 @@ fig.suptitle("Bulding a Squiggle with Activation Functions")
 activation_x = np.linspace(-32, 5, 101)
 data_x = np.linspace(0, 1, 101)
 
-draw_activation_part(activation_x, ax[0][0], softplus, WEIGHT_1, BIAS_1, "SoftPlus")
+draw_activation_part(activation_x, ax[0][0], softplus, WEIGHT_1, BIAS_1,
+                     "SoftPlus")
 draw_activation_part(activation_x, ax[1][0], relu, WEIGHT_1, BIAS_1, "Relu")
 # I don't have weights (from video abvove) for sigmoid or softmax
 # draw_activation_part(x, ax[2][0], sigmoid, WEIGHT#_1, BIAS_1, "Sigmoid")
 # draw_activation_part(x, ax[3][0], softmax, WEIGHT_1, BIAS_1, "Softmax")
 
-draw_activation_part(activation_x, ax[0][1], softplus, WEIGHT_2, BIAS_2, "SoftPlus")
-draw_activation_part(activation_x, ax[1][1], relu, WEIGHT_2, BIAS_2, "Relu")
+draw_activation_part(activation_x, ax[0][1], softplus, WEIGHT_2, BIAS_2,
+                     "SoftPlus")
+
+draw_activation_part(activation_x, ax[1][1], relu, WEIGHT_2, BIAS_2,
+                     "Relu")
+
 # I don't have weights (from video abvove) for sigmoid or softmax
 # draw_activation_part(x, ax[2][1], sigmoid, WEIGHT_2, BIAS_2, "Sigmoid")
 # draw_activation_part(x, ax[3][1], softmax, WEIGHT_2, BIAS_2, "Softmax")
@@ -93,9 +144,20 @@ draw_activation_part(activation_x, ax[1][1], relu, WEIGHT_2, BIAS_2, "Relu")
 display_portions_of_activation_functions(
     WEIGHT_1, BIAS_1, WEIGHT_2, BIAS_2, softplus, ax[0][2], data_x
 )
+draw_original_points(ax[0][2])
+
 display_portions_of_activation_functions(
     WEIGHT_1, BIAS_1, WEIGHT_2, BIAS_2, relu, ax[1][2], data_x
 )
+draw_original_points(ax[1][2])
+
+display_final_sums(WEIGHT_1, BIAS_1, WEIGHT_2, BIAS_2, softplus, ax[0][3],
+                   data_x, -1.3, 2.8)
+draw_original_points(ax[0][3])
+
+display_final_sums(WEIGHT_1, BIAS_1, WEIGHT_2, BIAS_2, relu, ax[1][3],
+                   data_x, -1.3, 2.8)
+draw_original_points(ax[1][3])
 
 plt.tight_layout()
 plt.show()
